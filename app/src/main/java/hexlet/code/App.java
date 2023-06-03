8abb7d5ad7ce3ea5cab22678f85231ae39f52747
Pavel Vinogradov <paul.vin.95@gmail.com>
2023-06-03 18:52:55 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,5 +3,6 @@ package hexlet.code;
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+        Cli.getUserName();
     }
 }
