e7e76fc54670a223b79c7c657be33bdd4ed05337
Pavel Vinogradov <paul.vin.95@gmail.com>
2023-06-04 19:13:59 +0300

    update App.java, add first game - Even

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet\n0 - Exit");
+        System.out.println("1 - Greet\n" +
+                "2 - Even\n" +
+                "0 - Exit");
         System.out.print("Your choice: ");
 
         Scanner scanner = new Scanner(System.in);
         String userChoice = scanner.next();
 
-        if (userChoice.equals("1")) {
-            System.out.println("Welcome to the Brain Games!");
-            Cli.getUserName();
-        } else if (userChoice.equals("0")) {
-            System.out.println("See you next time!");
-        } else {
-            System.out.println("You entered wrong number.");
+        switch (userChoice) {
+            case "0" -> System.out.println("See you next time!");
+            case "1" -> Cli.getUserName();
+            case "2" -> Even.isEvenGame();
+            default -> System.out.println("You entered wrong number.");
         }
     }
 }
