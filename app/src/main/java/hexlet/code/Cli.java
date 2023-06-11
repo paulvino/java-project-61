995ce74e9b827bdc33d0b7052adf7216948f86b3
Pavel Vinogradov <paul.vin.95@gmail.com>
2023-06-11 14:48:37 +0300

    update Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -8,7 +8,7 @@ class Cli {
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("May I have your name? ");
-        final String userName = scanner.next();
+        String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
     }
 }
