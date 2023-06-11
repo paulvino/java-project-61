0b9c98af2259c135d10cb36f9a0d543754e6687d
Pavel Vinogradov <paul.vin.95@gmail.com>
2023-06-11 17:16:14 +0300

    update Calculator.java

--- app/src/main/java/hexlet/games/Calculator.java
+++ app/src/main/java/hexlet/games/Calculator.java
@@ -17,23 +17,17 @@ public class Calculator {
         }
     }
 
-    public static int getFirstOperand() {
-        return (int) (Math.random() * 20);
-    }
-
-    public static int getSecondOperand() {
-        return (int) (Math.random() * 20);
-    }
-
     public static boolean checkAnswer(int operation, String userAnswer, int firstOperand, int secondOperand) {
+        long multiply = (long) firstOperand * secondOperand;
+        int subtraction = firstOperand - secondOperand;
+        int sum = firstOperand + secondOperand;
         switch (operation) {
             case 0:
-                if (userAnswer.equals(Long.toString((long) firstOperand * secondOperand))) {
+                if (userAnswer.equals(Long.toString(multiply))) {
                     Engine.resultGood();
                     return true;
                 } else {
-                    System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer "
-                            + "was '" + (firstOperand * secondOperand) + "'.");
+                    Engine.resultBad(userAnswer, Long.toString(multiply));
                     return false;
                 }
             case 1:
@@ -41,17 +35,15 @@ public class Calculator {
                     Engine.resultGood();
                     return true;
                 } else {
-                    System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer "
-                            + "was '" + (firstOperand - secondOperand) + "'.");
+                    Engine.resultBad(userAnswer, Integer.toString(subtraction));
                     return false;
                 }
             case 2:
-                if (userAnswer.equals(Integer.toString(firstOperand + secondOperand))) {
+                if (userAnswer.equals(Integer.toString(sum))) {
                     Engine.resultGood();
                     return true;
                 } else {
-                    System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer "
-                            + "was '" + (firstOperand + secondOperand) + "'.");
+                    Engine.resultBad(userAnswer, Integer.toString(sum));
                     return false;
                 }
             default:
