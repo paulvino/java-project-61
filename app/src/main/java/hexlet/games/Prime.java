6f5456ce5d7b3a54622a308d553ddbd26eeba081
Pavel Vinogradov <paul.vin.95@gmail.com>
2023-06-13 12:43:15 +0300

    add Prime.java (Prime game)

---
+++ app/src/main/java/hexlet/games/Prime.java
@@ -0,0 +1,38 @@
+package hexlet.games;
+
+import hexlet.code.Engine;
+
+public class Prime {
+
+    public static int askQuestion() {
+        int question = (int) (Math.random() * 100);
+        System.out.println("Question: " + question);
+        return question;
+    }
+
+    public static String correctAnswer(int question) {
+        int counter = 0;
+        for (int i = 1; i <= ((question / 2) + 1); i++) {
+            if (question % i == 0) {
+                counter++;
+            }
+
+            if (counter > 1) {
+                return "no";
+            }
+        }
+        return "yes";
+    }
+
+    public static boolean checkAnswer(int question, String userAnswer) {
+        String correctAnswer = Prime.correctAnswer(question);
+
+        if (correctAnswer.equals(userAnswer)) {
+            Engine.resultGood();
+            return true;
+        } else {
+            Engine.resultBad(userAnswer, correctAnswer);
+            return false;
+        }
+    }
+}
